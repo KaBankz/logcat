394f6ec0763a036659437aa458c5b54216a4ca27
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-09-11 11:26:00 -0400

    Format

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 Deno.serve(
   {
     onListen: (addr) => {
-      console.log('Listening on these addresses:');
+      console.log("Listening on these addresses:");
 
       const networkInterfaces = Deno.networkInterfaces();
 
       for (const ni of networkInterfaces) {
-        if (ni.name === 'lo0' || ni.name === 'en0') {
-          if (ni.family === 'IPv6') {
-            console.log(`\t%chttp://${ni.address}:${addr.port}`, 'color: red');
+        if (ni.name === "lo0" || ni.name === "en0") {
+          if (ni.family === "IPv6") {
+            console.log(`\t%chttp://${ni.address}:${addr.port}`, "color: red");
           } else {
             console.log(
               `\t%chttp://${ni.address}:${addr.port}`,
-              'color: green'
+              "color: green",
             );
           }
         }
@@ -20,7 +20,7 @@ Deno.serve(
     },
   },
   async (req) => {
-    if (req.method === 'POST' || req.method === 'PUT') {
+    if (req.method === "POST" || req.method === "PUT") {
       if (req.body) {
         const body = await req.text();
         console.log(body);
@@ -29,7 +29,7 @@ Deno.serve(
       return new Response();
     }
 
-    console.error('Invalid Method:', req.method);
-    return new Response('Invalid Method', { status: 405 });
-  }
+    console.error("Invalid Method:", req.method);
+    return new Response("Invalid Method", { status: 405 });
+  },
 );
